7d34c0aa3c6f73c3ec0376102479c175860575ec
Nakul Sabharwal <nasabhar@microsoft.com>
2019-03-12 15:20:53 +0530

    Added msgraph-sdk-java's IAuthProvider interface impl.

--- msgraph-sdk-android-auth/src/main/java/com/microsoft/graph/authentication/IMSALAuthenticationProvider.java
+++ msgraph-sdk-android-auth/src/main/java/com/microsoft/graph/authentication/IMSALAuthenticationProvider.java
@@ -2,8 +2,9 @@ package com.microsoft.graph.authentication;
 
 import android.content.Intent;
 import com.microsoft.graph.authentication.IAuthenticationProvider;
+import com.microsoft.graph.httpcore.ICoreAuthenticationProvider;
 
 
-public interface IMSALAuthenticationProvider extends IAuthenticationProvider {
+public interface IMSALAuthenticationProvider extends IAuthenticationProvider , ICoreAuthenticationProvider {
     public void handleInteractiveRequestRedirect(int requestCode, int resultCode, Intent data);
 }
